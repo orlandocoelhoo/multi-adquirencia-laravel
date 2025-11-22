5c386f1ed1f2dc101bca0053a78bb95aa3ec3181
Orlando Coelho <orlando.l.coelho@gmail.com>
2025-11-22 15:41:12 -0300

    Registro de usuario (Controller e Router)

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->group(function () {
+    Route::prefix('users')->group(function () {
+        Route::post('register', [RegisterController::class, 'register']);
+    });
+});
+
+
