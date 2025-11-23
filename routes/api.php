8fdcd6e7618632a4bf3f726dd8b9cbf416bc82cb
Orlando Coelho <orlando.l.coelho@gmail.com>
2025-11-23 19:05:06 -0300

    Criação rota de pagamento

--- routes/api.php
+++ routes/api.php
@@ -4,14 +4,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PaymentController;
 
 Route::prefix('v1')->group(function () {
-    Route::prefix('users')->group(function () {
-        Route::post('register', [RegisterController::class, 'register']);
-    });
+    Route::post('users/register', [RegisterController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
+
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('logout', [AuthController::class, 'logout']);
+        Route::post('payment', [PaymentController::class, 'process']);
     });
 });
 
