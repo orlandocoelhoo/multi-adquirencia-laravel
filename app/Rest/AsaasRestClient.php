b260761973123bc1fe03f8527a288b8bba27ee7f
Orlando Coelho <orlando.l.coelho@gmail.com>
2025-11-23 20:31:02 -0300

    ajustes no asaas

--- app/Rest/AsaasRestClient.php
+++ app/Rest/AsaasRestClient.php
@@ -7,7 +7,7 @@ class AsaasRestClient
     public function createPayment(array $paymentData): array
     {
         // Aqui seria a logica de intregração com a api do asaas utilizando cURL ou GuzzleHttp
-        // Simulação de chamada à API do Asaas
+        // Simulação de retorno da API Asaas
         return [
             'id' => 'asaas_payment_123',
             'status' => 'success',
--- src/core/Gateways/AsaasGateway.php
+++ src/core/Gateways/AsaasGateway.php
@@ -5,25 +5,31 @@ namespace Core\Gateways;
 use App\Rest\AsaasRestClient;
 use Core\Interfaces\PaymentGatewayInterface;
 use Core\UseCase\SavePayment;
+use Exception;
 
 class AsaasGateway implements PaymentGatewayInterface
 {
     public function processPayment(array $paymentData): array
     {
-        // Simulação de processamento de pagamento com Asaas
-        $asaasResponse = new AsaasRestClient();
-        $response = $asaasResponse->createPayment($paymentData);
+        try {
+            // Simulação de processamento de pagamento com Asaas
+            $asaasResponse = new AsaasRestClient();
+            $response = $asaasResponse->createPayment($paymentData);
 
-        $save = $this->save([
-            'name' => $paymentData['name'],
-            'amount' => $paymentData['amount'],
-            'installment' => $paymentData['installment'],
-            'dueDate' => $paymentData['due_date'],
-            'status' => $response['status'],
-            'rawResponse' => $response,
-        ]);
+            $save = $this->save([
+                'name' => $paymentData['name'],
+                'amount' => $paymentData['amount'],
+                'installment' => $paymentData['installment'],
+                'dueDate' => $paymentData['due_date'],
+                'status' => $response['status'],
+                'rawResponse' => $response,
+            ]);
 
-        return ['gateway' => 'Asaas', 'status' => $save];
+            return ['gateway' => 'Asaas', 'status' => $save];
+        } catch (Exception $e) {
+            throw new Exception("Error processing payment: " . $e->getMessage(), 1);
+        }
+
     }
 
     public function save($data): bool
