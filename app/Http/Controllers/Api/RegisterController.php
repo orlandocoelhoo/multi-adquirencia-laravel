793ad505e6c18b4701e8c33db033041ac5f81d02
Orlando Coelho <orlando.l.coelho@gmail.com>
2025-11-23 16:36:39 -0300

    Reorganização os arquivos

---
+++ app/Http/Controllers/Api/RegisterController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterUserRequest;
+use Core\Users\RegisterUser;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends Controller
+{
+    public function register(RegisterUserRequest $request)
+    {
+        $user = RegisterUser::execute(
+            $request->name,
+            $request->tenant_id,
+            $request->email,
+            $request->password
+        );
+
+        return response()->json(['message' => 'User register successful!', 'user' => $user], 201);
+    }
+}
