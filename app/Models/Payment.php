cb7ff734f7b83b3349ab00b0f50882214f364ad5
Orlando Coelho <orlando.l.coelho@gmail.com>
2025-11-23 20:16:40 -0300

    fillable payment

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    //
+    public $fillable = [
+        'user_id',
+        'gateway',
+        'name_on_card',
+        'amount',
+        'installment',
+        'due_date',
+        'status',
+        'raw_response',
+    ];
 }
