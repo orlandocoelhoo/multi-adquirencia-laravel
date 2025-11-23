afcabca954eaf9cee6d5caa8c654a2033b6fb47c
Orlando Coelho <orlando.l.coelho@gmail.com>
2025-11-23 16:38:36 -0300

    organização de rotas

--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\AuthController;
 
 Route::prefix('v1')->group(function () {
     Route::prefix('users')->group(function () {
         Route::post('register', [RegisterController::class, 'register']);
     });
-    Route::post('login', [\App\Http\Controllers\AuthController::class, 'login']);
+    Route::post('login', [AuthController::class, 'login']);
     Route::middleware('auth:sanctum')->group(function () {
-        Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+        Route::post('logout', [AuthController::class, 'logout']);
     });
 });
 
