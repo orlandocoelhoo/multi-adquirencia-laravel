fd214f2d8abd8edd850c0fae07930c4f2f7ed95f
Orlando Coelho <orlando.l.coelho@gmail.com>
2025-11-23 19:10:55 -0300

    Correção namespace RegisterUser

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterUserRequest;
-use Core\Users\RegisterUser;
+use Core\UseCase\Users\RegisterUser;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
